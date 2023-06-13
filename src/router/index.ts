f6dfa3ec5e42ebd341636b4eee7e7a1048bb561f
donnyha <donny.dh.ha@gmail.com>
2023-06-13 13:39:01 -0500

    Create routes for recipe generation in router/index.ts
    [RRG-2] Create the Hero Section
    - created meal route
    - created day route
    - created week route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import MealView from '../views/MealView.vue'
+import DayView from '../views/DayView.vue'
+import WeekView from '../views/WeekView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +11,21 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
+    },
+    {
+      path: '/meal',
+      name: 'meal',
+      component: MealView
+    },
+    {
+      path: '/day',
+      name: 'day',
+      component: DayView
+    },
+    {
+      path: '/week',
+      name: 'week',
+      component: WeekView
     },
   ]
 })
