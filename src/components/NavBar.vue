e60304bad95c2d54ea4fbeabdf2c4d900333b812
donnyha <donny.dh.ha@gmail.com>
2023-06-13 14:03:46 -0500

    Fix issue with mobile nav in Navbar.vue
    [RRG-2] Create the Hero Section
    - fix issue with mobile nav visibility
    - fix icons

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
   import { RouterLink } from 'vue-router'
   import { ref } from 'vue'
+  import { BoltIcon } from '@heroicons/vue/24/outline'
+  import { Bars3Icon } from '@heroicons/vue/24/outline'
+  import { XMarkIcon } from '@heroicons/vue/24/outline'
 
   const mobileMenuOpen = ref(false)
 </script>
@@ -9,10 +12,10 @@
   <!-- Desktop Nav -->
   <nav class="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
     <div class="flex lg:flex-1">
-      <a href="#" class="-m-1.5 p-1.5">
+      <RouterLink to="/" class="-m-1.5 p-1.5">
         <span class="sr-only">Random Recipe Generator</span>
-        <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
-      </a>
+        <BoltIcon class="h-8 w-auto" alt="logo" />
+      </RouterLink>
     </div>
     <div class="flex lg:hidden">
       <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700" @click="mobileMenuOpen = true">
@@ -32,14 +35,14 @@
   </nav>
 
   <!-- Mobile Nav -->
-  <div class="lg:hidden" @close="mobileMenuOpen = false" :open="mobileMenuOpen">
+  <div class="lg:hidden" v-show="mobileMenuOpen" @close="mobileMenuOpen = false">
     <div class="fixed inset-0 z-50" />
     <div class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
       <div class="flex items-center justify-between">
-        <a href="#" class="-m-1.5 p-1.5">
+        <RouterLink to="/" class="-m-1.5 p-1.5">
           <span class="sr-only">Your Company</span>
-          <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
-        </a>
+          <span class="sr-only">Random Recipe Generator</span>
+        </RouterLink>
         <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700" @click="mobileMenuOpen = false">
           <span class="sr-only">Close menu</span>
           <XMarkIcon class="h-6 w-6" aria-hidden="true" />
