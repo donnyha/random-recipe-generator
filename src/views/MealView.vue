dd26e08a147a189f2c131145f930a8ba4c044de8
donnyha <donny.dh.ha@gmail.com>
2023-07-04 17:31:59 -0500

    scaffold addIngredient() method in MealView.vue

--- src/views/MealView.vue
+++ src/views/MealView.vue
@@ -1,4 +1,27 @@
 <script setup lang="ts">
+  import { ref, type Ref } from 'vue'
+
+  let ingredients: Ref<string[]> = ref([])
+  let isIngredientsEmpty: Ref<boolean> = ref(true)
+  let newIngredient: Ref<string> = ref('')
+
+  const addIngredient = (ingredient: string) => {
+    // check if ingrdient is empty string
+    // check if ingredient already exists
+
+    ingredients.value.push(ingredient)
+    newIngredient.value = ''
+    checkIfIngredientsEmpty()
+  }
+  
+  const removeIngredient: (ingredientIndex: number) => void = (ingredientIndex: number) => {
+    ingredients.value.splice(ingredientIndex, 1)
+    checkIfIngredientsEmpty()
+  }
+
+  const checkIfIngredientsEmpty: () => void = () => {
+    ingredients.value.length > 0 ? isIngredientsEmpty.value = false : isIngredientsEmpty.value = true
+  }
 </script>
 
 <template>
@@ -13,14 +36,22 @@
             <label for="user-ingredients" class="block text-sm font-medium leading-6 text-gray-900">Ingredients</label>
             <div class="mt-2">
               <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                Empty
+                <div v-if="!isIngredientsEmpty">
+                  <ul>
+                    <li v-for="(ingredient, index) in ingredients" :key="index" >
+                      <input type="text" name="ingredient-{{ index }}" :value="ingredient" readonly />
+                      <button type="button" id="delete-btn-{{ index }}" @click="removeIngredient(index)">-</button>
+                    </li>
+                  </ul>
+                </div>
+                <div v-else>Empty</div>
               </div>
 
               <div class="flex flex-row">
                 <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                  <input type="text" name="ingredients" id="username" autocomplete="username" class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="e.g. tomato" />
+                  <input type="text" v-model="newIngredient" id="new-ingredient" name="new-ingredient" placeholder="e.g. tomato" class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" />
                 </div>
-                <button class="ml-2 btn">Add</button>
+                <button type="button" @click="addIngredient(newIngredient)" class="ml-2 btn">Add</button>
               </div>
 
             </div>
