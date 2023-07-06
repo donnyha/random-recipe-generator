3b3ee5c33a7cb3c4ec6eea2c908a0e1d48b545ee
donnyha <donny.dh.ha@gmail.com>
2023-07-06 16:10:07 -0500

    created logic for adding and removing ingredients

--- src/views/MealView.vue
+++ src/views/MealView.vue
@@ -3,15 +3,26 @@
 
   let ingredients: Ref<string[]> = ref([])
   let isIngredientsEmpty: Ref<boolean> = ref(true)
-  let newIngredient: Ref<string> = ref('')
+  let newIngredientInput: Ref<string> = ref('')
 
-  const addIngredient = (ingredient: string) => {
-    // check if ingrdient is empty string
-    // check if ingredient already exists
+  const addIngredient: (ingredient: string) => void = (ingredient: string) => {
+    const doesIngredientAlreadyExist = checkIfIngredientAlreadyExist(ingredient);
 
-    ingredients.value.push(ingredient)
-    newIngredient.value = ''
-    checkIfIngredientsEmpty()
+    if (ingredient.length !== 0 && !doesIngredientAlreadyExist) {
+      ingredients.value.push(ingredient)
+      newIngredientInput.value = ''
+      checkIfIngredientsEmpty()
+    }
+  }
+
+  const checkIfIngredientAlreadyExist: (newIngredient: string) => boolean = (newIngredient: string) => {
+    let doesExist: boolean = false;
+
+    if (ingredients.value.includes(newIngredient)) {
+      doesExist = true;
+    }
+
+    return doesExist
   }
   
   const removeIngredient: (ingredientIndex: number) => void = (ingredientIndex: number) => {
@@ -28,7 +39,7 @@
   <form>
     <div class="space-y-12">
       <div class="border-b border-gray-900/10 pb-12">
-        <h2 class="text-base font-semibold leading-7 text-gray-900">Recipe</h2>
+        <h2 class="text-base font-semibold leading-7 text-gray-900">Recipes</h2>
         <p class="mt-1 text-sm leading-6 text-gray-600">Please click 'Generate' Button to generate a random recipe</p>
 
         <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
@@ -49,9 +60,9 @@
 
               <div class="flex flex-row">
                 <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                  <input type="text" v-model="newIngredient" id="new-ingredient" name="new-ingredient" placeholder="e.g. tomato" class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" />
+                  <input type="text" v-model="newIngredientInput" id="new-ingredient" name="new-ingredient" placeholder="e.g. tomato" class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" />
                 </div>
-                <button type="button" @click="addIngredient(newIngredient)" class="ml-2 btn">Add</button>
+                <button type="button" @click="addIngredient(newIngredientInput)" class="ml-2 btn">Add</button>
               </div>
 
             </div>
