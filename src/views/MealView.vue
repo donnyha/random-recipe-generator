7cd57031c0b751155cf6b87e4e1621371335794c
donnyha <donny.dh.ha@gmail.com>
2023-07-14 22:41:36 -0500

    scaffold ingredient axios call

--- src/views/MealView.vue
+++ src/views/MealView.vue
@@ -4,6 +4,9 @@
   let ingredients: Ref<string[]> = ref([])
   let isIngredientsEmpty: Ref<boolean> = ref(true)
   let newIngredientInput: Ref<string> = ref('')
+  let isRecipesEmpty: Ref<boolean> = ref(true)
+  let recipes: Ref<string[]> = ref([])
+
 
   const addIngredient: (ingredient: string) => void = (ingredient: string) => {
     const doesIngredientAlreadyExist = checkIfIngredientAlreadyExist(ingredient);
@@ -33,6 +36,23 @@
   const checkIfIngredientsEmpty: () => void = () => {
     ingredients.value.length > 0 ? isIngredientsEmpty.value = false : isIngredientsEmpty.value = true
   }
+
+  const generateRecipes: () => void = () => {
+    // make axios call to get recipes based on ingredients
+    getRecipesBasedOnIngredients(ingredients.value)
+
+    // get and display recipes information
+      // foreach recipes 
+        // make axios call to get recipe information
+        // push recipe information to recipes
+      
+    // check if recipe in empty
+  }
+
+  const getRecipesBasedOnIngredients: (ingredients: string[]) => void = (ingredients: string[]) => {
+    console.log(ingredients);
+    
+  }
 </script>
 
 <template>
@@ -40,8 +60,14 @@
     <div class="space-y-12">
       <div class="border-b border-gray-900/10 pb-12">
         <h2 class="text-base font-semibold leading-7 text-gray-900">Recipes</h2>
-        <p class="mt-1 text-sm leading-6 text-gray-600">Please click 'Generate' Button to generate a random recipe</p>
-
+        <div v-if="isRecipesEmpty">
+          <p class="mt-1 text-sm leading-6 text-gray-600">Please click 'Generate' Button to generate a random recipe</p>
+        </div>
+        <div v-else>
+          <ul v-for="(recipe, index) in recipes" :key="index">
+            <li>{{ recipe }}</li>
+          </ul>
+        </div>
         <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
           <div class="sm:col-span-4">
             <label for="user-ingredients" class="block text-sm font-medium leading-6 text-gray-900">Ingredients</label>
@@ -72,7 +98,7 @@
     </div>
 
     <div class="mt-6 flex items-center justify-end gap-x-6">
-      <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
+      <button type="button" @click="generateRecipes()" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Generate</button>
     </div>
   </form>
 </template>
