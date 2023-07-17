97c05eb67ec1a45457d4820a073d5279c2da3112
donnyha <donny.dh.ha@gmail.com>
2023-07-17 17:25:14 -0500

    scaffold selected recipe show on click

--- src/views/MealView.vue
+++ src/views/MealView.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
-  import { ref, type Ref } from 'vue'
+  import { ref, type Ref, isProxy, toRaw } from 'vue'
+  import axios, { type AxiosRequestConfig } from 'axios'
 
   let ingredients: Ref<string[]> = ref([])
   let isIngredientsEmpty: Ref<boolean> = ref(true)
   let newIngredientInput: Ref<string> = ref('')
   let isRecipesEmpty: Ref<boolean> = ref(true)
-  let recipes: Ref<string[]> = ref([])
+  let recipes: Ref<any[]> = ref([])
+  let selectedRecipe: Ref<object> = ref({})
 
 
   const addIngredient: (ingredient: string) => void = (ingredient: string) => {
@@ -37,21 +39,50 @@
     ingredients.value.length > 0 ? isIngredientsEmpty.value = false : isIngredientsEmpty.value = true
   }
 
-  const generateRecipes: () => void = () => {
-    // make axios call to get recipes based on ingredients
-    getRecipesBasedOnIngredients(ingredients.value)
-
-    // get and display recipes information
-      // foreach recipes 
-        // make axios call to get recipe information
-        // push recipe information to recipes
-      
-    // check if recipe in empty
+  const checkIfRecipesEmpty: () => void = () => {
+    recipes.value.length > 0 ? isRecipesEmpty.value = false : isRecipesEmpty.value = true
   }
 
-  const getRecipesBasedOnIngredients: (ingredients: string[]) => void = (ingredients: string[]) => {
-    console.log(ingredients);
-    
+  const generateRecipes: () => void = async () => {
+    recipes.value = await getRecipesBasedOnIngredients(ingredients.value)
+    checkIfRecipesEmpty()
+  }
+
+  // @TODO refactor to different component
+  const getRecipesBasedOnIngredients: (ingredientsToSend: string[]) => Promise<object[] | any[]> = async (ingredientsToSend: string[]) => {
+    let recipes: any[] = []
+    const rawIngredients = checkForProxyAndReturnRaw(ingredientsToSend);
+    const url = 'https://api.spoonacular.com/recipes/findByIngredients'
+
+    await axios.get(url, {
+      params: {
+        apiKey: 'c9f2fb75c57145fd9ad43da15f293379',
+        ingredients: rawIngredients.join(',')
+      }
+    })
+    .then((response: AxiosRequestConfig) => {
+      recipes = response.data
+    })
+    .catch((error: Error) => {
+      console.log(error)
+    })
+
+    return recipes
+  }
+
+  const checkForProxyAndReturnRaw:(itemToCheck: any) => any = (itemToCheck: any) => {
+    let raw = itemToCheck
+
+    if (isProxy(itemToCheck)) {
+      raw = toRaw(itemToCheck)  
+    }
+
+    return raw
+  }
+
+  const showRecipeInformation: (recipe: object) => void = (recipe: object) => {
+    selectedRecipe.value = recipe
+    console.log(selectedRecipe.value);
   }
 </script>
 
@@ -60,14 +91,36 @@
     <div class="space-y-12">
       <div class="border-b border-gray-900/10 pb-12">
         <h2 class="text-base font-semibold leading-7 text-gray-900">Recipes</h2>
+
+        <!-- @TODO Refactor to separate component  -->
         <div v-if="isRecipesEmpty">
           <p class="mt-1 text-sm leading-6 text-gray-600">Please click 'Generate' Button to generate a random recipe</p>
         </div>
-        <div v-else>
-          <ul v-for="(recipe, index) in recipes" :key="index">
-            <li>{{ recipe }}</li>
-          </ul>
+        <div v-else class="flex flex-row">
+          <div>
+            <h3>Recipes</h3>
+            <ul v-for="(recipe, index) in recipes" :key="index">
+              <li>
+                <button type="button" @click="showRecipeInformation(recipe)">
+                  {{ recipe.title }}
+                </button>
+              </li>
+            </ul>
+          </div>
+          
+          <div>
+            <h3>Information</h3>
+            <!-- <div v-show="typeof selectedRecipe !== null">
+              <h4>Title: {{ selectedRecipe.title }}</h4>
+              <p>Missed Ingredients</p>
+              <ul v-for="(index, missedIngredient) in selectedRecipe.missedIngredients" :key="index">
+                <li>{{ missedIngredient.originalName }} ({{ missedIngredient.amount }} {{ missedIngredient.unit }})</li>
+              </ul>
+            </div> -->
+
+          </div>
         </div>
+
         <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
           <div class="sm:col-span-4">
             <label for="user-ingredients" class="block text-sm font-medium leading-6 text-gray-900">Ingredients</label>
