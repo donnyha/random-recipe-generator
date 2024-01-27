f0ac5c6000fcf7e3c10352e2eb539908aafbb001
donnyha <donny.dh.ha@gmail.com>
2024-01-27 13:32:33 -0600

    clean up layout

--- src/views/MealView.vue
+++ src/views/MealView.vue
@@ -2,17 +2,16 @@
   import { ref, type Ref, isProxy, toRaw } from 'vue'
   import axios, { type AxiosRequestConfig } from 'axios'
   import type Recipe from '../types/Recipe';
-  import { CheckCircleIcon, XCircleIcon } from '@heroicons/vue/24/outline';
+  import { CheckCircleIcon, MinusCircleIcon, ExclamationCircleIcon } from '@heroicons/vue/24/outline';
 
-  let ingredients: Ref<string[]> = ref([])
-  let isIngredientsEmpty: Ref<boolean> = ref(true)
-  let newIngredientInput: Ref<string> = ref('')
-  let isRecipesEmpty: Ref<boolean> = ref(true)
-  let beingUsedIngredients: Ref<string> = ref('')
-  let recipes: Ref<any[]> = ref([])
-  let selectedRecipe: Ref<Recipe | any> = ref({})
-  let selectedRecipeInstructions: Ref<object[] | any[]> = ref([])
-
+  let ingredients: Ref<string[]> = ref([]);
+  let isIngredientsEmpty: Ref<boolean> = ref(true);
+  let newIngredientInput: Ref<string> = ref('');
+  let isRecipesEmpty: Ref<boolean> = ref(true);
+  let beingUsedIngredients: Ref<string> = ref('');
+  let recipes: Ref<any[]> = ref([]);
+  let selectedRecipe: Ref<Recipe | any> = ref({});
+  let selectedRecipeInstructions: Ref<object[] | any[]> = ref([]);
 
   const addIngredient: (ingredient: string) => void = (ingredient: string) => {
     const doesIngredientAlreadyExist = checkIfIngredientAlreadyExist(ingredient);
@@ -130,33 +129,49 @@
           <h1 class="text-xl font-bold tracking-tight text-gray-900 sm:text-5xl">Meal</h1>
         </div>
 
-        <!-- Ingredients -->
-        <div id="ingredients-section" class="flex items-center justify-center h-full m-10">
-          <div class="flex flex-row sm:col-span-4 mt-2 parent">
-
-            <!-- Ingredients List -->
-            <div id="ingredients-list-section child">
-              <label for="user-ingredients" class="block text-sm font-medium leading-6 text-gray-900">Ingredients</label>
-              <div v-if="!isIngredientsEmpty" class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                <ul>
-                  <li v-for="(ingredient, index) in ingredients" :key="index" >
-                    <input type="text" name="ingredient-{{ index }}" :value="ingredient" readonly />
-                    <button type="button" id="delete-btn-{{ index }}" @click="removeIngredient(index)">-</button>
-                  </li>
-                </ul>
-              </div>
-              <div v-else>No Ingredients</div>
-            </div>
-            
-            <!-- New Ingredient Input -->
+        <!-- Ingredients Section -->
+        <div id="ingredients-section" class="flex flex-col items-center justify-center h-full m-10">
+          
+          <!-- New Ingredient Section -->
+          <div class="flex flex-row sm:col-span-4 mt-2">
             <div id="new-ingredients-section" class="flex flex-row">
               <div class="rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md max-h-8">
-                <input type="text" v-model="newIngredientInput" id="new-ingredient" name="new-ingredient" placeholder="e.g. tomato" class="block border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6 w-32 h-8 overflow-hidden" />
+                <input 
+                  type="text" 
+                  v-model="newIngredientInput"
+                  @keypress.enter="addIngredient(newIngredientInput)" 
+                  id="new-ingredient" 
+                  name="new-ingredient" 
+                  placeholder="e.g. tomato" 
+                  class="block border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6 w-32 h-8 overflow-hidden" 
+                />
               </div>
-              <button type="button" @click="addIngredient(newIngredientInput)" class="max-h-8 ml-2 btn">Add</button>
+              <button type="button" @click="addIngredient(newIngredientInput)" class="max-h-8 ml-5 btn rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Add</button>
             </div>
-
           </div>
+
+          <!-- Ingredients List -->
+          <div id="ingredients-list-section">
+            <!-- <label for="user-ingredients" class="block text-sm font-medium leading-6 text-gray-900">Ingredients</label> -->
+            <div v-if="!isIngredientsEmpty" class="flex sm:max-w-md">
+              <ul class="mt-10">
+                <li v-for="(ingredient, index) in ingredients" :key="index" class="flex items-center mb-3">
+                  <input type="text" name="ingredient-{{ index }}" :value="ingredient" class="p-0 mr-2 w-full border-none cursor-default pointer-events-none" readonly />
+                  <button type="button" id="delete-btn-{{ index }}" @click="removeIngredient(index)" class="border-none">
+                    <MinusCircleIcon class="h-6 w-auto stroke-red-400" />
+                  </button>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+        </div>
+
+        <!-- Generate Recipes Button -->
+        <div class="flex items-center justify-center h-full mb-10">
+          <button id="generate-button" type="button" @click="generateRecipes()" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+            Generate
+          </button>
         </div>
 
         <!-- @TODO Refactor to separate component  -->
@@ -164,12 +179,29 @@
         <div id="recipes-section" v-if="!isRecipesEmpty" class="flex flex-row mx-40">
           <!-- Recipe List -->
           <div id="recipe-list" class="mr-10 w-1/3">
-            <h2 class="text-base font-semibold leading-7 text-gray-900">
+            <h2 class="text-base font-bold leading-7 text-gray-900 mb-5">
               Recipes<span v-if="beingUsedIngredients.length > 0"> with {{ beingUsedIngredients }}</span> 
             </h2>
             <ul v-for="(recipe, index) in recipes" :key="index">
-              <li class="mb-3">
-                <button type="button" @click="showRecipeInformation(recipe)" class="text-left">
+              <li v-if="selectedRecipe.title === recipe.title" class="mb-3">
+                <button type="button" @click="showRecipeInformation(recipe)" class="text-left font-bold text-indigo-500">
+                  {{ recipe.title }}
+                </button>
+              </li>
+              <li v-else class="mb-3">
+                <button 
+                  type="button" 
+                  @click="showRecipeInformation(recipe)" 
+                  class="
+                    text-left 
+                    transition ease-in-out 
+                    delay-150 
+                    hover:-translate-y-1 
+                    hover:scale-110 
+                    hover:text-indigo-500 
+                    duration-300
+                  "
+                >
                   {{ recipe.title }}
                 </button>
               </li>
@@ -178,42 +210,51 @@
 
           <!-- Selected Recipe Information -->
           <div id="recipe-info" v-if="Object.keys(selectedRecipe).length > 0" class="w-2/3">
-            <h2>Information</h2>
+            <h2 class="font-bold mb-5">Recipe Information</h2>
             <div>
-              <h3 class="mb-2">Name: {{ selectedRecipe.title }}</h3>
-              <p>Ingredients</p>
+              <h3 class="mb-5">Name: {{ selectedRecipe.title }}</h3>
 
               <!-- Existing Ingredients-->
-              <div v-if="Object.keys(selectedRecipe.usedIngredients).length > 0">
-                <ul v-for="(requiredIngredients, index) in selectedRecipe.usedIngredients" :key="index">
-                  <li class="flex flex-row">{{ requiredIngredients.original }} <CheckCircleIcon class="h-6 w-auto stroke-green-400" /></li>
-                </ul>
+              <div id="selected-recipe-ingredients" class="mb-5">
+                
+                <h4>Ingredients</h4>
+
+                <div v-if="Object.keys(selectedRecipe.usedIngredients).length > 0">
+                  <ul v-for="(requiredIngredients, index) in selectedRecipe.usedIngredients" :key="index" class="list-disc">
+                    <li class="ml-5">
+                      <span class="flex flex-row">
+                        <span>{{ requiredIngredients.original }}</span> 
+                        <CheckCircleIcon class="h-6 w-auto stroke-green-400" />
+                      </span>
+                    </li>
+                  </ul>
+                </div>
+
+                <!-- Missing Ingredients -->
+                <div v-if="Object.keys(selectedRecipe.missedIngredients).length > 0" class="mb-2">
+                  <ul v-for="(missedIngredient, index) in selectedRecipe.missedIngredients" :key="index" class="list-disc">
+                    <li class="ml-5">
+                      <span class="flex flex-row">
+                        <span class="text-gray-400">{{ missedIngredient.original }}</span> 
+                        <ExclamationCircleIcon class="h-6 w-auto stroke-yellow-400" />
+                      </span>
+                    </li>
+                  </ul>
+                </div>
               </div>
 
-              <!-- Missing Ingredients -->
-              <div v-if="Object.keys(selectedRecipe.missedIngredients).length > 0" class="mb-2">
-                <ul v-for="(missedIngredient, index) in selectedRecipe.missedIngredients" :key="index">
-                  <li class="flex flex-row">
-                    <p class="text-gray-400 line-through">{{ missedIngredient.original }}</p> <XCircleIcon class="h-6 w-auto stroke-red-400" /></li>
-                </ul>
-              </div>
             </div>
 
             <!-- Instructions -->
             <div v-if="selectedRecipeInstructions.length > 0">
-              <h3>Instructions</h3>
-              <ul>
-                <li v-for="(instruction, index) in selectedRecipeInstructions" :key="index">
-                  {{ instruction.number }}. {{ instruction.step }}
+              <h4>Instructions</h4>
+              <ul class="list-decimal">
+                <li v-for="(instruction, index) in selectedRecipeInstructions" :key="index" class="ml-8 mb-2">
+                  {{ instruction.step }}
                 </li>
               </ul>
             </div>
           </div>
-        </div>
-
-        <!-- Generate Button -->
-        <div class="flex items-center justify-center h-full">
-          <button id="generate-button" type="button" @click="generateRecipes()" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Generate</button>
         </div>
 
       </div>
